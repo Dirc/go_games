b76ad39c75dceb8452112c4e036c9e9a91e43b42
Dirc <e.e.cornet@gmail.com>
2018-10-08 16:47:03 +0200

    PONG: score, game state, bugfixes

--- pong/pong.go
+++ pong/pong.go
@@ -2,8 +2,13 @@ package main
 
 // Features
 //
-// - framerate
-// - Score
+// - paddle stay on screen
+// - ball cannot bounce behind paddle..
+//
+// - improve gameplay
+//   - paddle angle bouncing
+//   - Ball start in random direction
+//   - Ball velocity increases
 // - Game over state
 // - two player or PC
 // - AI needs to be inperfect
@@ -11,6 +16,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -20,13 +26,93 @@ import (
 //
 const winWidth, winHeight int = 800, 600
 
-var ballStartPos pos = pos{float32(winWidth / 2), float32(winHeight / 2)}
-var ballStartXV float32 = 5
-var ballStartYV float32 = 5
+// Enum
+type gameState int
 
-var paddleVelocity float32 = 5
+const (
+	start gameState = iota
+	play
+	gameover
+)
 
-var score1, score2 int = 0, 0
+var state = start
+
+// ---- end enum
+
+var ballStartVelocity float32 = 250
+var paddleSpeed float32 = 250
+
+var nums = [][]byte{
+	{
+		1, 1, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+		1, 0, 0,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 0, 1,
+		1, 0, 1,
+		1, 1, 1,
+		0, 0, 1,
+		0, 0, 1,
+	},
+	{
+		1, 1, 1,
+		1, 0, 0,
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		1, 0, 0,
+		1, 1, 1,
+		1, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		0, 0, 1,
+		0, 0, 1,
+		0, 0, 1,
+	},
+	{
+		1, 1, 1,
+		1, 0, 1,
+		1, 1, 1,
+		1, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		1, 0, 1,
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+	}}
 
 //
 // Structures and Functions
@@ -37,6 +123,35 @@ type color struct {
 
 type pos struct {
 	x, y float32
+}
+
+func getCenter() pos {
+	return pos{float32(winWidth / 2), float32(winHeight / 2)}
+}
+
+func drawNumber(pos pos, color color, size int, num int, array_of_pixels []byte) {
+	startX := int(pos.x) - (size*3)/2
+	startY := int(pos.y) - (size*5)/2
+
+	for i, v := range nums[num] {
+		if v == 1 {
+			for y := startY; y < startY+size; y++ {
+				for x := startX; x < startX+size; x++ {
+					setPixel(x, y, color, array_of_pixels)
+				}
+			}
+		}
+		startX += size
+		if (i+1)%3 == 0 {
+			startY += size
+			startX -= size * 3
+		}
+	}
+}
+
+// lerp: standard helper function to position objects on the screen
+func lerp(a float32, b float32, pct float32) float32 {
+	return a + pct*(b-a)
 }
 
 type ball struct {
@@ -57,29 +172,34 @@ func (ball *ball) draw(array_of_pixels []byte) {
 	}
 }
 
-func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle, score1 int, score2 int) {
-	ball.x += ball.xv
-	ball.y += ball.yv
+func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle, elapsedTime float32) {
+	ball.x += ball.xv * elapsedTime
+	ball.y += ball.yv * elapsedTime
 
 	// Collisions
 	// Collision: Top/bottom screen
 	if ball.y-ball.radius < 0 || ball.y+ball.radius > float32(winHeight) {
+		// Change direction
 		ball.yv = -ball.yv
 	}
 	// Collision: left/right screen - score
 	if ball.x < 0 {
-		ball.x = ballStartPos.x
-		score2++
-	}
-	if int(ball.x) > winWidth {
-		ball.y = ballStartPos.y
-		score1++
+		rightPaddle.score++
+		ball.pos = getCenter()
+		state = start
+	} else if int(ball.x) > winWidth {
+		leftPaddle.score++
+		ball.pos = getCenter()
+		state = start
 	}
 	// Collision: left paddle
 	if ball.x-ball.radius < leftPaddle.x+leftPaddle.w/2 {
 		// ball between paddle bottom and paddle top
 		if ball.y+ball.radius < leftPaddle.y+leftPaddle.h/2 && ball.y-ball.radius > leftPaddle.y-leftPaddle.h/2 {
 			ball.xv = -ball.xv
+			// Bugfix1: Ensure the ball bouncens so that the above statement for x is not true again.
+			// Bug1: Ball could move inside the paddle if the statement keeps being thue for x.
+			ball.x = leftPaddle.x + leftPaddle.w/2.0 + ball.radius
 		}
 	}
 	// Collision: right paddle
@@ -87,6 +207,7 @@ func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle, score1 int, sc
 		// ball between paddle bottom and paddle top
 		if ball.y+ball.radius < rightPaddle.y+rightPaddle.h/2 && ball.y-ball.radius > rightPaddle.y-rightPaddle.h/2 {
 			ball.xv = -ball.xv
+			ball.x = rightPaddle.x - rightPaddle.w/2.0 - ball.radius
 		}
 	}
 }
@@ -95,6 +216,8 @@ type paddle struct {
 	pos
 	w     float32
 	h     float32
+	speed float32
+	score int
 	color color
 }
 
@@ -107,18 +230,23 @@ func (paddle *paddle) draw(array_of_pixels []byte) {
 			setPixel(startX+x, startY+y, paddle.color, array_of_pixels)
 		}
 	}
+
+	numX := lerp(paddle.x, getCenter().x, 0.2)
+	drawNumber(pos{numX, 35}, paddle.color, 10, paddle.score, array_of_pixels)
 }
 
-func (paddle *paddle) update(keyState []uint8) {
+//func drawPaddle
+
+func (paddle *paddle) update(keyState []uint8, elapsedTime float32) {
 	if keyState[sdl.SCANCODE_UP] != 0 {
-		paddle.y -= paddleVelocity
+		paddle.y -= paddle.speed * elapsedTime
 	}
 	if keyState[sdl.SCANCODE_DOWN] != 0 {
-		paddle.y += paddleVelocity
+		paddle.y += paddle.speed * elapsedTime
 	}
 }
 
-func (paddle *paddle) aiUpdate(ball *ball) {
+func (paddle *paddle) aiUpdate(ball *ball, elapsedTime float32) {
 	paddle.y = ball.y
 
 }
@@ -181,10 +309,10 @@ func main() {
 		}
 	}
 
-	player1 := paddle{pos{30, float32(winHeight / 2)}, 20, 100, color{255, 255, 255}}
-	player2 := paddle{pos{float32(winWidth - 30), float32(winHeight / 2)}, 20, 100, color{255, 255, 255}}
+	player1 := paddle{pos{30, float32(winHeight / 2)}, 20, 100, paddleSpeed, 0, color{255, 255, 255}}
+	player2 := paddle{pos{float32(winWidth - 30), float32(winHeight / 2)}, 20, 100, paddleSpeed, 0, color{255, 255, 255}}
 
-	ball := ball{ballStartPos, 20, ballStartXV, ballStartYV, color{255, 255, 255}}
+	ball := ball{getCenter(), 20, ballStartVelocity, ballStartVelocity, color{255, 255, 255}}
 
 	// Keyboard input
 	// Define variable for keyState
@@ -199,23 +327,37 @@ func main() {
 
 	keyState := sdl.GetKeyboardState() // array of bytes "uint8", is used in paddle.update()
 
+	var frameStart time.Time
+	var elapsedTime float32
+
 	// Game loop
 	// Ends with quit event
 	for {
+		frameStart = time.Now()
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
 				return
 			}
 		}
+
+		// Update
+		if state == play {
+			player1.update(keyState, elapsedTime)
+			player2.aiUpdate(&ball, elapsedTime)
+			ball.update(&player1, &player2, elapsedTime)
+		} else if state == start {
+			if keyState[sdl.SCANCODE_SPACE] != 0 {
+				if player1.score == 3 || player2.score == 3 {
+					player1.score = 0
+					player2.score = 0
+				}
+				state = play
+			}
+		}
+
+		// Draw
 		clear(array_of_pixels)
-
-		//fmt.Println(score1, score2)
-
-		player1.update(keyState)
-		player2.aiUpdate(&ball)
-		ball.update(&player1, &player2, score1, score2)
-
 		player1.draw(array_of_pixels)
 		player2.draw(array_of_pixels)
 		ball.draw(array_of_pixels)
@@ -224,7 +366,14 @@ func main() {
 		renderer.Copy(tex, nil, nil)
 		renderer.Present()
 
-		sdl.Delay(16)
+		elapsedTime = float32(time.Since(frameStart).Seconds())
+		// Naive framerate
+		//sdl.Delay(16)
+		// Improved framerate
+		if elapsedTime < .005 {
+			sdl.Delay(5 - uint32(elapsedTime/1000.0))
+			elapsedTime = float32(time.Since(frameStart).Seconds())
+		}
 	}
 
 }
